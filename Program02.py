7fe326dbc60af10b65ea83816a376de66899ec35
devendradaradra-2776 <devendradaradra@gmail.com>
2026-08-15 14:47:07 +0530

    Add files via upload

---
+++ Program02.py
@@ -0,0 +1,7 @@
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+if a < b:
+    print("Minimum number is:", a)
+else:
+    print("Minimum number is:", b)
---
+++ Program06.py
@@ -0,0 +1,6 @@
+ch = input("Enter a single character: ").lower()
+
+if ch in ['a', 'e', 'i', 'o', 'u']:
+    print("Vowel")
+else:
+    print("Consonant")
